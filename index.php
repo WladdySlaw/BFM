31cfad35d9556458a582e56f97045cbd2d0c90d0
Wladdy Slaw <videoprompt@gmail.com>
2016-12-04 23:38:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="ru"> <!--<![endif]-->
+<?php
+	define("RS","/apteka/");
+	$meta_title = "Главная";
+?>
+<?php include("inc/head.php"); ?>
+<body>
+	<?php include("inc/header.php"); ?>
+	<section class="bigger_apteka">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="apteka__info_block">
+					<div class="col-xs-12">
+						<div class="center_block">
+							<p class="center_block__top_text">Мы - больше чем аптека</p>
+							<div class="center_block__line"></div>
+							<p class="center_block__top_text center_block__top_text2">больше<br>чем здоровье</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="about_us">
+		<div class="container">
+			<div class="row">
+				<div class="about_us_block">
+					<div class="col-sm-12 about_us_title_block">
+						<div class="about_us_title wow fadeInLeft"><p>О нас</p></div>
+					</div>
+					<div class="about_us__info">
+						<div class="col-sm-7 wow fadeInLeft">
+							<p class="about_us__parag">Сеть фармамаркетов "BFM" начала свою работу на рынке Украины в августе 2006 года. Первые торговые точки были открыты компанией в Киеве, с последующим развитием этой сети на всей территории Украины. На сегодня «Аптека Доброго Дня» представлена почти во всех областях Украины и насчитывает более ста пятидесяти аптек.</p>
+							<p class="about_us__parag">Национальная фармацевтическая сеть «BFM» - это удобный формат аптечного сервиса: сочетание традиций классической аптеки с быстрым и высококачественным обслуживанием маркета.</p>
+							<p class="about_us__parag">Аптеки имеют открытую зону самообслуживания, благодаря которой покупатели имеют возможность самостоятельно выбрать подходящий товар для здоровья, получив при этом квалифицированную консульта</p>
+						</div>
+						<div class="col-sm-5 osmotr_come_block wow fadeInRight">
+							<div class="osmotr_come">
+								<div class="osmotrTextBlock">
+									<p class="osmotrText" id="osmotrText">задай вопрос</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12 icons_info">
+							<div class="col-sm-3 icons_info__block wow fadeInLeft">
+								<div class="icons__info_icon"></div>
+								<div class="icons__info_line"></div>
+								<p class="icons__info_number">11 000</p>
+									<p class="icons__info_text">ТОВАРОВ <br>ДЛЯ ЗАКАЗА</p>
+							</div>
+							<div class="col-sm-3 icons_info__block wow fadeInLeft">
+								<div class="icons__info_icon2"></div>
+								<div class="icons__info_line"></div>
+								<p class="icons__info_number">17 500</p>
+									<p class="icons__info_text">клиентов <br>в месяц</p>
+							</div>
+							<div class="col-sm-3 icons_info__block wow fadeInLeft">
+								<div class="icons__info_icon3"></div>
+								<div class="icons__info_line"></div>
+								<p class="icons__info_number">100%</p>
+									<p class="icons__info_text">сертифицировання <br>продукция</p>
+							</div>
+							<div class="col-sm-3 icons_info__block wow fadeInLeft">
+								<div class="icons__info_icon4"></div>
+								<div class="icons__info_line"></div>
+								<p class="icons__info_number">11 000</p>
+									<p class="icons__info_text">специалистов<br>работают для вас<br>в сети BFM</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="slider wow zoomIn">
+		<div id="owl-demo" class="owl-carousel owl-theme">
+  			<div class="item">
+					<div class="carousel__items">
+						<div class="carouselPic"></div>
+						<div class="carousel__text">
+							<div class="reviewerInfo">
+								<p class="reviewerName">Дмитрий Анатольевич</p>
+								<p class="reviewerPost">врач терапевт</p>
+							</div>
+							<div class="quote_1"></div>
+							<p class="carousel__main_text">Очень хорошая аптека! Вежливые заботливые сотрудники. Полный ассортимент! Хорошие скидки. Быстрая доставка прямо домой! Согласна с предыдущим отзывом: отсутствие платежного терминала у курьера затрудняет оплату.</p>
+							<div class="quote_2"></div>
+						</div>
+						<div class="sliderImg"></div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="carousel__items">
+						<div class="carouselPic carouselPic2"></div>
+						<div class="carousel__text">
+							<div class="reviewerInfo">
+								<p class="reviewerName">Дмитрий Анатольевич</p>
+								<p class="reviewerPost">врач терапевт</p>
+							</div>
+							<div class="quote_1"></div>
+							<p class="carousel__main_text">Очень хорошая аптека! Вежливые заботливые сотрудники. Полный ассортимент! Хорошие скидки. Быстрая доставка прямо домой! Согласна с предыдущим отзывом: отсутствие платежного терминала у курьера затрудняет оплату.</p>
+							<div class="quote_2"></div>
+						</div>
+						<div class="sliderImg"></div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="carousel__items">
+						<div class="carouselPic carouselPic3"></div>
+						<div class="carousel__text">
+							<div class="reviewerInfo">
+								<p class="reviewerName">Дмитрий Анатольевич</p>
+								<p class="reviewerPost">врач терапевт</p>
+							</div>
+							<div class="quote_1"></div>
+							<p class="carousel__main_text">Очень хорошая аптека! Вежливые заботливые сотрудники. Полный ассортимент! Хорошие скидки. Быстрая доставка прямо домой! Согласна с предыдущим отзывом: отсутствие платежного терминала у курьера затрудняет оплату.</p>
+							<div class="quote_2"></div>
+						</div>
+						<div class="sliderImg"></div>
+					</div>
+				</div>
+		</div>
+	</section>
+	<section class="why__we">
+		<div class="container">
+			<div class="row">
+					<div class="col-md-6 why__we_wrap wow slideInLeft">
+						<div class="col-md-4">
+							<div class="why__we_pic">
+								<p class="why__we_num">1</p>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<p class="why__we_title">Профессионализм</p>
+							<div class="why__we_line"></div>
+							<p class="why__we_text">В нашей команде профессиональные фармацевты и провизоры</p>
+						</div>
+					</div>
+					<div class="col-md-6 why__we_wrap wow slideInRight">
+						<div class="col-md-4">
+							<div class="why__we_pic why__we_pic2">
+								<p class="why__we_num">2</p>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<p class="why__we_title">Большой выбор</p>
+							<div class="why__we_line"></div>
+							<p class="why__we_text">Широкий ассортимент, как отечественных, так и зарубежных препаратов</p>
+						</div>
+					</div>
+					<div class="col-md-6 why__we_wrap wow slideInLeft">
+						<div class="col-md-4">
+							<div class="why__we_pic why__we_pic3">
+								<p class="why__we_num">3</p>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<p class="why__we_title">Доставка</p>
+							<div class="why__we_line"></div>
+							<p class="why__we_text">различные Удобные для вас варианты доставки</p>
+						</div>
+					</div>
+					<div class="col-md-6 why__we_wrap wow slideInRight">
+						<div class="col-md-4">
+							<div class="why__we_pic why__we_pic5">
+								<p class="why__we_num">4</p>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<p class="why__we_title">консультация 24/7</p>
+							<div class="why__we_line"></div>
+							<p class="why__we_text">мы предоставляем квалифицированную консультацию в любое время</p>
+						</div>
+					</div>
+			</div>
+		</div>
+	</section>
+	<?php include("inc/footer.php"); ?>
+	<div class="right__layer"></div>
+	<!--Modal window-->
+	<?php include("inc/modal.php"); ?>
+	<?php include("inc/modalKorzina.php"); ?>
+	<?php include("inc/modalKorzinaEmpty.php"); ?>
+	<?php include("inc/modalEnter.php"); ?>
+	<?php include("inc/modalForget.php"); ?>
+	<div class="hidden"></div>
+	<?php include("inc/_js.php"); ?>
+	<?php include("inc/preloader.php"); ?>
+</body>
+</html>
